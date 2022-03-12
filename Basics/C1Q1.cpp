5849a0231df27ee1a0d43acc5ce49d6e38fef489
Zeyd17 <92731099+Zeyd17@users.noreply.github.com>
2022-03-12 22:31:50 +0530

    Add files via upload

---
+++ Basics/C1Q1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	float gallons;
+	cout<<"Enter the number of gallons"<<endl;
+	cin>>gallons;
+	
+	gallons /= 7.481;
+	
+	cout<<gallons<<"Cubic feet";
+}
---
+++ Basics/C1Q2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"1990		135"<<endl
+	<<"1991		7290"<<endl
+	<<"1992		11300"<<endl
+	<<"1993		16200"<<endl;
+	
+	return 0;
+	}
